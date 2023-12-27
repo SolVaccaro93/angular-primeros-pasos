040c042cee852c3550a5b3e694409d3846e19140
Sol Vaccaro <vaccaro.marisol93@gmail.com>
2023-12-27 16:38:54 -0300

    Fin de seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,38 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+  public name : string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void {
+    this.name = 'Spiderman'
+    throw  'Metodo no implementado'
+
+  }
+
+  changeAge():void{
+   this.age = 25;
+   throw 'Metodo no implementado'
+  }
+
+  resetForm():void{
+   this.name = 'Ironman';
+   this.age  = 45;
+  }
+  }
+
+
